65cfa588523c8a2c57cceb487d3bea6078962352
Anthony Vettraino <apvett09@yahoo.com>
2024-09-15 15:41:46 -0400

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
-initScrollReveal(targetElements, defaultProps);
-initTiltEffect();
-
 window.addEventListener("load", () => {
   initScrollReveal(targetElements, defaultProps);
   initTiltEffect();
-  ScrollReveal().sync(); // Force an update if needed
 });
